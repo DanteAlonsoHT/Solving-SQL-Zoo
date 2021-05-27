349a7e098567ba90197ae735d2f110be613cdf97
DanteAlonsoHT <dante1818@hotmail.com>
2021-05-26 19:46:37 -0500

    Add SELECT from world

--- SELECT_quiz.sql
+++ SELECT_quiz.sql
@@ -38,3 +38,56 @@ WHERE area > 50000 AND population < 10000000
 SELECT name, population/area
 FROM world
 WHERE name IN ('China', 'Nigeria', 'France', 'Australia')
+
+/* SELECT from world */
+
+SELECT name, continent, population FROM world
+
+SELECT name
+FROM world
+WHERE population > 200000000
+
+SELECT name, gdp/population
+FROM world
+WHERE population >= 200000000
+
+SELECT name, population/1000000
+FROM world
+WHERE continent = 'South America'
+
+SELECT name, population
+FROM world
+WHERE name IN ('France', 'Germany', 'Italy')
+
+SELECT name
+FROM world
+WHERE name LIKE '%United%'
+
+SELECT name, population, area
+FROM world
+WHERE area > 3000000 OR population > 250000000
+
+SELECT name, population, area
+FROM world
+WHERE area > 3000000 AND NOT population > 250000000 OR 
+NOT area > 3000000 AND population > 250000000
+
+SELECT name, ROUND(population/1000000, 2), ROUND(GDP/1000000000,2)
+FROM world
+WHERE continent = 'South America'
+
+SELECT name, ROUND(gdp/population,-3)
+FROM world
+WHERE gdp > 1000000000000
+
+SELECT name, capital
+FROM world
+WHERE LEN(name) = LEN(capital)
+
+SELECT name, capital
+FROM world
+WHERE LEFT(name,1) = LEFT(capital,1) AND NOT name = capital
+
+SELECT name
+FROM world
+WHERE name LIKE '%a%' AND name LIKE '%e%' AND name LIKE '%i%' AND name LIKE '%o%' AND name LIKE '%u%' AND NOT name LIKE '% %'
