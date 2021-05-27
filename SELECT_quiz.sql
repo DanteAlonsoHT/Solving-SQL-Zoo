40652fc3bb5145c93b5d295c26170ebe584ee9cf
DanteAlonsoHT <dante1818@hotmail.com>
2021-05-27 17:22:38 -0500

    Add quiz 3

--- SELECT_quiz.sql
+++ SELECT_quiz.sql
@@ -120,3 +120,35 @@ WHERE name IN ('Cuba', 'Togo')
 SELECT name FROM world
 WHERE continent = 'South America'
 AND population > 40000000
+
+/* Quiz 3 */
+
+SELECT winner FROM nobel
+WHERE winner LIKE 'C%' AND winner LIKE '%n'
+
+SELECT COUNT(subject) FROM nobel
+WHERE subject = 'Chemistry'
+AND yr BETWEEN 1950 and 1960
+
+SELECT COUNT(DISTINCT yr) FROM nobel
+WHERE yr NOT IN (SELECT DISTINCT yr FROM nobel WHERE subject = 'Medicine')
+
+SELECT subject, winner FROM nobel WHERE winner LIKE 'Sir%' AND yr LIKE '196%'
+
+SELECT yr FROM nobel
+WHERE yr NOT IN(SELECT yr 
+FROM nobel
+WHERE subject IN ('Chemistry','Physics'))
+
+SELECT DISTINCT yr
+FROM nobel
+WHERE subject='Medicine' 
+AND yr NOT IN(SELECT yr FROM nobel 
+WHERE subject='Literature')
+AND yr NOT IN (SELECT yr FROM nobel
+WHERE subject='Peace')
+
+SELECT subject, COUNT(subject) 
+FROM nobel 
+WHERE yr ='1960' 
+GROUP BY subject
