c569cd3839a8574bdff9a651ca8c9d923a6c2a98
DanteAlonsoHT <dante1818@hotmail.com>
2021-05-28 17:29:40 -0500

    Add quiz to MORE JOIN

--- MORE JOIN.sql
+++ MORE JOIN.sql
@@ -85,3 +85,32 @@ SELECT id
 FROM actor
 WHERE name = 'Art Garfunkel'))
 AND NOT actor.name = 'Art Garfunkel'
+
+/* Quiz */
+
+SELECT name
+FROM actor INNER JOIN movie ON actor.id = director
+WHERE gross < budget
+
+SELECT *
+FROM actor JOIN casting ON actor.id = actorid
+JOIN movie ON movie.id = movieid
+
+SELECT name, COUNT(movieid)
+FROM casting JOIN actor ON actorid=actor.id
+WHERE name LIKE 'John %'
+GROUP BY name ORDER BY 2 DESC
+
+SELECT title 
+FROM movie JOIN casting ON (movieid=movie.id)
+JOIN actor   ON (actorid=actor.id)
+WHERE name='Paul Hogan' AND ord = 1
+
+SELECT name
+FROM movie JOIN casting ON movie.id = movieid
+JOIN actor ON actor.id = actorid
+WHERE ord = 1 AND director = 351
+
+SELECT title, yr 
+FROM movie, casting, actor 
+WHERE name='Robert De Niro' AND movieid=movie.id AND actorid=actor.id AND ord = 3
