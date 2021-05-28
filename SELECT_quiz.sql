923d08233afc91e0dc1fb01d87c33cee57dafe7d
DanteAlonsoHT <dante1818@hotmail.com>
2021-05-28 00:22:39 -0500

    Add SELECT in SELECT

--- SELECT_quiz.sql
+++ SELECT_quiz.sql
@@ -152,3 +152,52 @@ SELECT subject, COUNT(subject)
 FROM nobel 
 WHERE yr ='1960' 
 GROUP BY subject
+
+/* SELECT in SELECT */
+
+SELECT name FROM world
+WHERE population >
+(SELECT population FROM world
+WHERE name='Russia')
+
+SELECT name FROM world
+WHERE continent = 'Europe' AND gdp/population > (
+SELECT gdp/population FROM world
+WHERE name = 'United Kingdom')
+
+SELECT name, continent FROM world
+WHERE continent IN (SELECT continent FROM world 
+WHERE name IN ('Argentina ','Australia'))
+
+SELECT name, population FROM world
+WHERE population > (SELECT population FROM world WHERE name = 'Canada') AND
+population < (SELECT population FROM world WHERE name = 'Poland')
+
+SELECT name, 
+CONCAT  (CAST (100*population/(SELECT population 
+FROM world 
+WHERE name 
+='Germany') AS DECIMAL(5,0)),'%') as  percentage
+FROM world
+WHERE continent = 'Europe';
+
+SELECT name
+FROM world
+WHERE gdp> ALL(SELECT gdp FROM world WHERE continent = 'Europe' AND gdp > 0)
+
+SELECT continent, name, area FROM world x
+WHERE area > ALL
+(SELECT area FROM world y
+WHERE y.area > x.area AND y.continent = x.continent) AND area > 0
+
+SELECT continent, name FROM world x
+WHERE name < ALL
+(SELECT name FROM world y
+WHERE x.name != y.name AND  y.continent = x.continent)
+
+SELECT name, continent, population FROM world x
+WHERE 25000000 > ALL(SELECT population FROM world y WHERE x.continent = y.continent)
+
+SELECT name, continent FROM world x
+WHERE population > ALL(SELECT population*3 FROM world y WHERE x.continent = y.continent
+AND x.name <> y.name)
