b1edb7cfbc14b1e1b1add57269ec58139947882f
DanteAlonsoHT <dante1818@hotmail.com>
2021-05-28 14:08:28 -0500

    Add quiz in JOIN.sql

--- JOIN.sql
+++ JOIN.sql
@@ -51,6 +51,34 @@ ON game.id = goal.matchid
 GROUP BY mdate, matchid, team1, team2
 ORDER BY mdate, matchid, team1, team2;
 
+/* Quiz  */
 
+game  JOIN goal ON (id=matchid)
 
+matchid, teamid, player, gtime, id, teamname, coach
 
+SELECT player, teamid, COUNT(*)
+FROM game JOIN goal ON matchid = id
+WHERE (team1 = "GRE" OR team2 = "GRE")
+AND teamid != 'GRE'
+GROUP BY player, teamid
+
+SELECT DISTINCT teamid, mdate
+FROM goal JOIN game on (matchid=id)
+WHERE mdate = '9 June 2012'
+
+SELECT DISTINCT player, teamid 
+FROM game JOIN goal ON matchid = id 
+WHERE stadium = 'National Stadium, Warsaw' 
+AND (team1 = 'POL' OR team2 = 'POL')
+AND teamid != 'POL'
+
+SELECT DISTINCT player, teamid, gtime
+FROM game JOIN goal ON matchid = id
+WHERE stadium = 'Stadion Miejski (Wroclaw)'
+AND (( teamid = team2 AND team1 != 'ITA') OR ( teamid = team1 AND team2 != 'ITA'))
+
+SELECT teamname, COUNT(*)
+FROM eteam JOIN goal ON teamid = id
+GROUP BY teamname
+HAVING COUNT(*) < 3
