f088546de13f8379b3f767505f8198126482ba42
DanteAlonsoHT <dante1818@hotmail.com>
2021-05-30 16:10:32 -0500

    Add quiz for Using NULL

--- Using NULL.sql
+++ Using NULL.sql
@@ -30,3 +30,32 @@ FROM teacher;
 
 SELECT name, CASE WHEN dept IN (1,2) THEN 'Sci' WHEN dept = 3 THEN 'Art' ELSE 'None' END
 FROM teacher;
+
+/* Quiz */
+
+SELECT teacher.name, dept.name FROM teacher LEFT OUTER JOIN dept ON (teacher.dept = dept.id)
+
+SELECT dept.name FROM teacher JOIN dept ON (dept.id = teacher.dept) WHERE teacher.name = 'Cutflower'
+
+SELECT dept.name, COUNT(teacher.name) FROM teacher RIGHT JOIN dept ON dept.id = teacher.dept GROUP BY dept.name
+
+/*
+Using SELECT name, dept, COALESCE(dept, 0) AS result FROM teacher on teacher table will:
+display 0 in result column for all teachers without department
+*/
+
+SELECT name,
+CASE WHEN phone = 2752 THEN 'two'
+WHEN phone = 2753 THEN 'three'
+WHEN phone = 2754 THEN 'four'
+END AS digit
+FROM teacher /*'four' for Throd*/
+
+SELECT name, 
+CASE 
+WHEN dept 
+IN (1) 
+THEN 'Computing' 
+ELSE 'Other' 
+END 
+FROM teacher
