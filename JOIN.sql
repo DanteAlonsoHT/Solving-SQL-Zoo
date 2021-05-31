26042aa14f8f55fd7f9ef7ebfca72ef0b613ee8d
DanteAlonsoHT <dante1818@hotmail.com>
2021-05-31 10:36:44 -0500

    Add enumeration for every SQL file in the repository

--- JOIN.sql
+++ JOIN.sql
@@ -1,51 +1,77 @@
+-- 1
+
 SELECT matchid, player FROM goal 
 WHERE teamid LIKE '%GER'
+
+-- 2
 
 SELECT id,stadium,team1,team2
 FROM game
 WHERE id = 1012
 
+-- 3
+
 SELECT player, teamid, stadium, mdate 
-FROM game JOIN goal ON (id=matchid) AND teamid LIKE 'GER'
+FROM game JOIN goal ON (id = matchid) AND teamid LIKE 'GER'
+
+-- 4
 
 SELECT team1, team2, player 
-FROM game JOIN goal ON (id=matchid) AND player LIKE 'Mario%'
+FROM game JOIN goal ON (id = matchid) AND player LIKE 'Mario%'
+
+-- 5
 
 SELECT player, teamid, coach, gtime
 FROM goal JOIN eteam ON teamid = id
-WHERE gtime<=10 
+WHERE gtime <= 10 
+
+-- 6
 
 SELECT mdate, teamname
-FROM game JOIN eteam ON (team1=eteam.id)
+FROM game JOIN eteam ON (team1 = eteam.id)
 WHERE coach LIKE 'Fernando Santos'
 
+-- 7
+
 SELECT player
-FROM game JOIN goal ON (id=matchid) AND stadium LIKE 'National Stadium, Warsaw'
+FROM game JOIN goal ON (id = matchid) AND stadium LIKE 'National Stadium, Warsaw'
+
+-- 8
 
 SELECT DISTINCT player
 FROM game JOIN goal ON matchid = id 
-WHERE (team2='GER' OR team1 = 'GER') AND NOT (teamid = 'GER')
+WHERE (team2 ='GER' OR team1 = 'GER') AND NOT (teamid = 'GER')
+
+-- 9
 
 SELECT teamname, COUNT(eteam.id) FROM goal
-JOIN eteam ON (goal.teamid= eteam.id)
+JOIN eteam ON (goal.teamid = eteam.id)
 GROUP BY teamname
+
+-- 10
 
 SELECT stadium, COUNT(matchid) FROM goal
 JOIN game ON matchid = game.id
 GROUP BY stadium
+
+-- 11
 
 SELECT matchid,mdate, COUNT(teamid)
 FROM game JOIN goal ON matchid = id 
 WHERE (team1 = 'POL' OR team2 = 'POL')
 GROUP BY matchid, mdate
 
+-- 12
+
 SELECT matchid, mdate, COUNT (teamid)
 FROM goal JOIN game ON matchid = id
 WHERE teamid = 'GER'
 GROUP BY matchid, mdate
 
-SELECT mdate, team1, SUM(CASE WHEN teamid=team1 THEN 1 ELSE 0 END) AS score1,
-team2, SUM(CASE WHEN teamid=team2 THEN 1 ELSE 0 END) AS score2
+-- 13
+
+SELECT mdate, team1, SUM(CASE WHEN teamid = team1 THEN 1 ELSE 0 END) AS score1,
+team2, SUM(CASE WHEN teamid = team2 THEN 1 ELSE 0 END) AS score2
 FROM game LEFT JOIN goal
 ON game.id = goal.matchid
 GROUP BY mdate, matchid, team1, team2
@@ -53,9 +79,15 @@ ORDER BY mdate, matchid, team1, team2;
 
 /* Quiz  */
 
+-- 1
+
 game  JOIN goal ON (id=matchid)
 
+-- 2
+
 matchid, teamid, player, gtime, id, teamname, coach
+
+-- 3
 
 SELECT player, teamid, COUNT(*)
 FROM game JOIN goal ON matchid = id
@@ -63,9 +95,13 @@ WHERE (team1 = "GRE" OR team2 = "GRE")
 AND teamid != 'GRE'
 GROUP BY player, teamid
 
+-- 4
+
 SELECT DISTINCT teamid, mdate
 FROM goal JOIN game on (matchid=id)
 WHERE mdate = '9 June 2012'
+
+-- 5
 
 SELECT DISTINCT player, teamid 
 FROM game JOIN goal ON matchid = id 
@@ -73,10 +109,14 @@ WHERE stadium = 'National Stadium, Warsaw'
 AND (team1 = 'POL' OR team2 = 'POL')
 AND teamid != 'POL'
 
+-- 6
+
 SELECT DISTINCT player, teamid, gtime
 FROM game JOIN goal ON matchid = id
 WHERE stadium = 'Stadion Miejski (Wroclaw)'
 AND (( teamid = team2 AND team1 != 'ITA') OR ( teamid = team1 AND team2 != 'ITA'))
+
+-- 7
 
 SELECT teamname, COUNT(*)
 FROM eteam JOIN goal ON teamid = id
--- Self  JOIN.sql
+++ Self  JOIN.sql
@@ -1,23 +1,35 @@
+-- 1 
+
 SELECT COUNT(id)
 FROM stops
+
+-- 2
 
 SELECT id
 FROM stops
 WHERE name = 'Craiglockhart'
 
+-- 3
+
 SELECT id, name 
 FROM stops JOIN route ON id = stop
 WHERE company = 'LRT' AND num = '4';
+
+-- 4
 
 SELECT company, num, COUNT(*)
 FROM route WHERE stop = 149 OR stop = 53
 GROUP BY company, num
 HAVING COUNT(*) = 2;
 
+-- 5
+
 SELECT r1.company, r1.num, r1.stop, r2.stop
 FROM route r1 JOIN route r2 ON
 (r1.company = r2.company AND r1.num = r2.num)
 WHERE r1.stop = 53 AND r2.stop = 149;
+
+-- 6
 
 SELECT r1.company, r1.num, stop1.name, stop2.name
 FROM route r1 JOIN route r2 ON
@@ -26,6 +38,8 @@ JOIN stops stop1 ON (r1.stop = stop1.id)
 JOIN stops stop2 ON (r2.stop = stop2.id)
 WHERE stop1.name = 'Craiglockhart' and stop2.name = 'London Road';
 
+-- 7
+
 SELECT distinct r1.company, r1.num
 FROM route r1 JOIN route r2 ON
 (r1.company = r2.company AND r1.num = r2.num)
@@ -33,12 +47,16 @@ JOIN stops stop1 ON (r1.stop = stop1.id)
 JOIN stops stop2 ON (r2.stop = stop2.id)
 WHERE stop1.id = 115 and stop2.id = 137;
 
+-- 8
+
 SELECT r1.company, r1.num
 FROM route r1 JOIN route r2 ON
 (r1.company = r2.company AND r1.num = r2.num)
 JOIN stops stop1 ON (r1.stop = stop1.id)
 JOIN stops stop2 ON (r2.stop = stop2.id)
 WHERE stop1.name = 'Craiglockhart' and stop2.name = 'Tollcross';
+
+-- 9
 
 SELECT DISTINCT name, r1.company, r2.num
 FROM route r1
@@ -48,17 +66,23 @@ WHERE r2.stop = 53;
 
 /* Quiz */
 
+-- 1
+
 SELECT DISTINCT a.name, b.name
 FROM stops a JOIN route z ON a.id=z.stop
 JOIN route y ON y.num = z.num
 JOIN stops b ON y.stop=b.id
 WHERE a.name='Craiglockhart' AND b.name ='Haymarket'
 
+-- 2
+
 SELECT S2.id, S2.name, R2.company, R2.num
 FROM stops S1, stops S2, route R1, route R2
 WHERE S1.name='Haymarket' AND S1.id=R1.stop
 AND R1.company=R2.company AND R1.num=R2.num
 AND R2.stop=S2.id AND R2.num='2A'
+
+-- 3
 
 SELECT a.company, a.num, stopa.name, stopb.name
 FROM route a JOIN route b ON (a.company=b.company AND a.num=b.num)
